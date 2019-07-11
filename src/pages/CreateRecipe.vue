5e21cf172b688a42d05403b6e844b22870a10054
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2019-07-11 00:23:20 -0400

    create recipe

--- src/pages/CreateRecipe.vue
+++ src/pages/CreateRecipe.vue
@@ -1,10 +1,53 @@
 <template>
-  <div></div>
+  <q-page padding class="flex flex-center">
+    <q-card style="width:50%">
+      <q-card-section>
+        <q-input v-model="name" label="Name"></q-input>
+        <q-input v-model="url" label="URL"></q-input>
+        <q-input v-model="dishType" label="Dish Type"></q-input>
+        <q-btn @click="createRecipe" class="q-mt-md full-width">Create</q-btn>
+      </q-card-section>
+    </q-card>
+  </q-page>
 </template>
 
 <script>
+import gql from "graphql-tag";
+
 export default {
-  name: "CreateRecipe"
+  name: "CreateRecipe",
+
+  data() {
+    return {
+      name: "",
+      url: "",
+      dishType: ""
+    };
+  },
+
+  methods: {
+    async createRecipe() {
+      console.log("Creating new recipe");
+      const response = await this.$apollo.mutate({
+        mutation: gql`
+          mutation($recipeInput: RecipeInput!) {
+            createRecipe(recipeInput: $recipeInput) {
+              id
+            }
+          }
+        `,
+        variables: {
+          recipeInput: {
+            name: this.name,
+            url: this.url,
+            dishType: this.dishType
+          }
+        }
+      });
+
+      console.log("Created recipe id:", response.data.createRecipe.id);
+    }
+  }
 };
 </script>
 
