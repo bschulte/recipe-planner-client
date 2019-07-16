e68f56742d05cb3af5647102b6e187f3e8829c29
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2019-07-16 10:00:29 -0400

    import chrome bookmarks

---
+++ src/components/CreateRecipe/ImportChromeBookmarks.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-card>
+    <q-card-section>
+      <strong>Import Chrome Bookmarks</strong>
+    </q-card-section>
+
+    <q-card-section>
+      <q-input
+        @input="
+          val => {
+            file = val[0];
+          }
+        "
+        filled
+        accept=".html"
+        type="file"
+        hint="Bookmarks HTML File"
+      />
+    </q-card-section>
+
+    <q-card-section>
+      <q-btn
+        color="primary"
+        @click="importBookmarks"
+        class="full-width"
+        :disable="file === null"
+      >
+        Import
+      </q-btn>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: "ImportChromeBookmarks",
+
+  data() {
+    return {
+      file: null
+    };
+  },
+
+  methods: {
+    importBookmarks() {
+      console.log("File:", this.file);
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped></style>
