1bdbc2d5ba3b6b30c249bb879c913953e08fb8e7
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2019-07-16 10:00:37 -0400

    import chrome bookmarks

--- src/pages/CreateRecipe.vue
+++ src/pages/CreateRecipe.vue
@@ -1,16 +1,19 @@
 <template>
-  <q-page padding class="flex flex-center">
+  <q-page padding class="flex flex-center column">
     <recipe-input></recipe-input>
+    <import-chrome-bookmarks class="q-mt-lg"></import-chrome-bookmarks>
   </q-page>
 </template>
 
 <script>
 import RecipeInput from "../components/CreateRecipe/RecipeInput";
+import ImportChromeBookmarks from "../components/CreateRecipe/ImportChromeBookmarks";
 
 export default {
   name: "CreateRecipe",
   components: {
-    RecipeInput
+    RecipeInput,
+    ImportChromeBookmarks
   }
 };
 </script>
